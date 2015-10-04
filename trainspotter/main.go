d0e75e32ee642a3cd184985d8b1693c5163853cf
Chris Reeves <hello@chris.reeves.io>
2015-10-04 15:40:13 +0100

    Connect and consumer from National Rail STOMP Feed

--- trainspotter/main.go
+++ trainspotter/main.go
@@ -7,7 +7,10 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"gopkg.in/stomp.v1"
 )
+
+const TOPIC string = "/topic/TD_LNE_GN_SIG_AREA"
 
 func flags() *pflag.FlagSet {
 	// Create flagset
@@ -40,12 +43,25 @@ func cli(flags *pflag.FlagSet, run func(cmd *cobra.Command, args []string)) *cob
 	return cmd
 }
 
-func main() {
-	// Create CLI
-	c := cli(flags(), func(cmd *cobra.Command, args []string) { // Method called when Tamzin is invoked
-		fmt.Println("Hello World")
+func run(cmd *cobra.Command, args []string) {
+	// Connect to STOMP
+	conn := fmt.Sprintf("%v:%v", viper.GetString("addr"), viper.GetString("port"))
+	s, _ := stomp.Dial("tcp", conn, stomp.Options{
+		Login:    viper.GetString("user"),
+		Passcode: viper.GetString("key"),
 	})
 
-	// Invoke CLI
+	// Subscribe to the topoc
+	sub, _ := s.Subscribe(TOPIC, stomp.AckClient)
+
+	// Get messages
+	for {
+		msg := <-sub.C
+		fmt.Println(string(msg.Body[:]))
+	}
+}
+
+func main() {
+	c := cli(flags(), run)
 	c.Execute()
 }
